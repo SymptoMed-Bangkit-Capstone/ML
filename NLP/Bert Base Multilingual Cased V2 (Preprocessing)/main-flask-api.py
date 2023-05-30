9ac864efc3cc925d43617a049ba6b73db48a9b8d
riobastian09 <riobastian1717@gmail.com>
2023-05-30 23:08:07 +0700

    Commit Ke-4 (Rio) : Update Fast API and the Requirements

---
+++ NLP/Bert Base Multilingual Cased V2 (Preprocessing)/main-flask-api.py
@@ -0,0 +1,40 @@
+import os
+import uvicorn
+import numpy as np
+from enum import Enum
+import tensorflow as tf
+from pydantic import BaseModel
+from transformers import TextClassificationPipeline
+from fastapi import FastAPI, HTTPException, Path, Query
+from transformers import BertTokenizer, BertForSequenceClassification
+
+app = FastAPI()  # create a new FastAPI app instance
+port = int(os.getenv("PORT"))
+
+# Define a Pydantic model for an item
+class Item(BaseModel):
+    query:str
+
+tokenizer = BertTokenizer.from_pretrained("./content/tokenizer_bert_multilungual_v2", local_files_only=True)    # sesuaikan dengan nama folder tokenizer yang sudah diupload
+model = BertForSequenceClassification.from_pretrained("./content/model_bert_multilungual_v2", from_tf=True)     # sesuaikan dengan nama folder model yang sudah diupload
+pipe = TextClassificationPipeline(model=model, tokenizer=tokenizer, top_k = 24)
+
+def predict(input):
+    text = str(input).lower()
+    pred = pipe(text)
+    kelas = pred[0][0]['label']
+    prob = pred[0][0]['score']
+    output = "Terdiagnosa sebagai : " + str(kelas)+ ", dengan probabilitas : " +str(round((prob)*100, 2))+'%'
+    return(output)
+
+@app.get("/")
+def hello_world():
+    return ("hello world")
+
+@app.post("/")
+def add_item(item: Item):
+    hasil = predict(item.query)
+    return {hasil}
+
+if __name__ == '__main__':
+    uvicorn.run(app, host="0.0.0.0", port=port, timeout_keep_alive=1200)
